106c4ffb6e336ae3cbb01fb2689e6026933e13d9
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2026-02-15 17:58:58 -0300

    Add project

---
+++ src/main/java/br/com/sales/micro/SalesMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.sales.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sales/micro/SalesMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sales.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
