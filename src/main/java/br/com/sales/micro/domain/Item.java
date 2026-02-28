04d2306e1db52a05c8406cf4ba1a3d4705dec038
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2026-02-27 21:42:08 -0300

    Fix: Update Domain structure

---
+++ src/main/java/br/com/sales/micro/domain/Item.java
@@ -0,0 +1,8 @@
+package br.com.sales.micro.domain;
+
+public class Item {
+    private String product_id;
+    private String product_name;
+    private Double product_price;
+    private Integer product_quantity;
+}
