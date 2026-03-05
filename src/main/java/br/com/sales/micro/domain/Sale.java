e73fac1fbc8d4f55b9e60338dd5715cc214ee99a
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2026-03-05 18:00:31 -0300

    Feature: Adds client class to mapping on de sale class

--- src/main/java/br/com/sales/micro/domain/Sale.java
+++ src/main/java/br/com/sales/micro/domain/Sale.java
@@ -19,10 +19,10 @@ import java.util.List;
 public class Sale {
     @Id
     private String id;
-    private String clientId;
     private Status status;
     private LocalDateTime date;
     private Double totalValue;
+    private Client client;
     private List<Item> items;
 
     @CreatedDate
