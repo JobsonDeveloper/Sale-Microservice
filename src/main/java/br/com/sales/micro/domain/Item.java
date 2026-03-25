bcea1e590b703c2a8062ab52a49f58956ef3fbaa
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2026-03-25 20:39:46 -0300

    Feature: Store and provide more product details

--- src/main/java/br/com/sales/micro/domain/Item.java
+++ src/main/java/br/com/sales/micro/domain/Item.java
@@ -7,7 +7,9 @@ import lombok.Setter;
 @Setter
 public class Item {
     private String id;
+    private String name;
     private Long barCode;
+    private String description;
+    private Integer quantity;
     private Double value;
-    private Long quantity;
 }
--- src/main/java/br/com/sales/micro/dto/request/ProductBasicInfoDto.java
+++ src/main/java/br/com/sales/micro/dto/request/ProductBasicInfoDto.java
@@ -2,6 +2,6 @@ package br.com.sales.micro.dto.request;
 
 public record ProductBasicInfoDto(
         Long productBarCode,
-        Long productQuantity
+        Integer productQuantity
 ) {
 }
