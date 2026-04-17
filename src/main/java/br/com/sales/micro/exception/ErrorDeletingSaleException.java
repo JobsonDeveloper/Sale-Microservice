c23edb9a1136799eb47b9ea296195c01b2dd5a80
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2026-04-17 17:20:13 -0300

    Refactor: Remove the unnecessary exceptions and update the processes of mapping errors

--- src/main/java/br/com/sales/micro/exception/ErrorDeletingSaleException.java
+++
@@ -1,8 +0,0 @@
-package br.com.sales.micro.exception;
-
-public class ErrorDeletingSaleException extends RuntimeException {
-    public ErrorDeletingSaleException()  {super("It was not possible to delete the sale!");}
-    public ErrorDeletingSaleException(String message) {
-        super(message);
-    }
-}
--- src/main/java/br/com/sales/micro/exception/SaleAlreadyCancelledException.java
+++
@@ -1,8 +0,0 @@
-package br.com.sales.micro.exception;
-
-public class SaleAlreadyCancelledException extends RuntimeException {
-    public SaleAlreadyCancelledException() {super("The sale has already been cancelled previously!");}
-    public SaleAlreadyCancelledException(String message) {
-        super(message);
-    }
-}
